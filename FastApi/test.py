a214f7dd414d3b9e83cea1f3f773e8b309096f59
Muhammad-Musharraf <muhammadmusharraf868@gmail.com>
2026-07-30 05:07:13 +0500

    Add files

--- FastApi/test.py
+++
@@ -1,9 +0,0 @@
-# tests/test_api.py
-from fastapi.testclient import TestClient
-from FastApi.main import app
-
-client = TestClient(app)
-
-def test_health():
-    resp = client.get("/health")
-    assert resp.status_code == 200
